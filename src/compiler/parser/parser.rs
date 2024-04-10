43635fd38e67aa2e6aebc1256eeb4e6eeae62bee
Matheus Dias de Souza <MatheusDiasDeSouzaDS@gmail.com>
2024-04-10 09:56:30 -0300

    Update parser.rs

--- src/compiler/parser/parser.rs
+++ src/compiler/parser/parser.rs
@@ -219,6 +219,8 @@ impl<'input> Parser<'input> {
             while self.token.0 != Token::Eof {
                 if let Some(id) = self.consume_identifier(reserved_words)? {
                     return Ok(id);
+                } else {
+                    self.next()?;
                 }
             }
             Err(ParsingFailure)
@@ -236,6 +238,8 @@ impl<'input> Parser<'input> {
         while self.token.0 != Token::Eof {
             if self.consume_context_keyword(name)? {
                 return Ok(());
+            } else {
+                self.next()?;
             }
         }
         Err(ParsingFailure)
